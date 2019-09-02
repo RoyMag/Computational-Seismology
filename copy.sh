62c42a7161b20ee94a799d1fdd0cfd81fec4156c
Subhadeep Roy <subhadeeproy29@gmail.com>
2019-09-02 19:52:56 +0530

    Delete copy.sh

--- copy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for stn in $(cat stn_XW.txt)
-do
-cp -rf /home/magneto/Thesis/tarimBasin/XW_1997-2001/goodEvents.txt ~/Thesis/tarimBasin/XW_1997-2001/station/${stn}
-cd ~/Thesis/tarimBasin/XW_1997-2001/station/${stn}
-echo "sh" "~/Thesis/newScripts/move.sh" | sh
-done
